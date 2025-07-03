99878785750684f52a3a508a713c2fc7f0478363
ABDUL KADER MAHI <145624870+akmahi024@users.noreply.github.com>
2025-07-03 14:30:57 +0900

    Greetings Added

--- DiceGame.java
+++ DiceGame.java
@@ -1,4 +1,5 @@
 import java.util.Random;
+import java.util.Scanner;
 
 public class DiceGame {
     public static void main(String[] args) {
@@ -18,5 +19,7 @@ public class DiceGame {
         System.out.println("Die 1: " + die1);
         System.out.println("Die 2: " + die2);
         System.out.println("Total value: " + total);
+        
+        scanner.close();
     }
 }
